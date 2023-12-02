382fcfa2ef8db20fe0f3a5c26f845715ef0c1a26
DzymFardreamer <87277885+DzymFardreamer@users.noreply.github.com>
2023-12-02 17:35:03 +0100

    Added example

---
+++ examples/run_absolute_motor.py
@@ -0,0 +1,37 @@
+import can 
+import time
+from mks_servo import MksServo
+
+import logging
+
+# Stock slcan firmware on Windows
+bus = can.interface.Bus(bustype='slcan', channel='COM3', bitrate=500000)
+notifier = can.Notifier(bus, [])
+
+def wait_for_motor_idle2(timeout):    
+    start_time = time.perf_counter()
+    while (time.perf_counter() - start_time < timeout) and servo.is_motor_running():
+        print(servo.read_motor_speed(), flush=True)
+        time.sleep(0.1)  # Small sleep to prevent busy waiting
+    return servo.is_motor_running()
+
+def move_motor(absolute_position):  
+    print(f"Moving motor to absolute position {absolute_position}", flush=True)
+    print(servo.run_motor_absolute_motion_by_axis(600, 0, absolute_position), flush=True)
+    wait_for_motor_idle2(30)
+    value = servo.read_encoder_value_addition()['value']
+    error = absolute_position - value
+    print(f"Movement at {absolute_position} with error {error}")    
+    print(f"", flush=True)
+    print()
+
+servo = MksServo(bus, notifier, 1)
+
+print(servo.set_work_mode(MksServo.WorkMode.SrvFoc))
+print(servo.set_subdivision(16))
+print(servo.set_working_current(2000))
+print(servo.set_current_axis_to_zero())
+
+while True:
+    move_motor(0x4000*13)
+    move_motor(0)
