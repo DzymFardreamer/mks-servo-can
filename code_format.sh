e1c57f4fe460359812d5450184bd3a13c1c5e215
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2025-02-11 14:00:51 +0200

    Up to version="0.2.1"

--- code_format.sh
+++ code_format.sh
@@ -1,6 +1,14 @@
 ### Install:
 # pip install ruff
-### Run in GIT BASH:
+
+### Run in GIT BASH as admin:
 # cd <path to>/mks-servo-can
 # ./code_format.sh
+
 ruff format --line-length 200
+
+### Install the lib from local (otional)
+pip install .
+
+### Install the lib from GiHhub
+# pip install git+https://github.com/DzymFardreamer/mks-servo-can.git
